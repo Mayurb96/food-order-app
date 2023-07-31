6d4a909c9ab7b9873f29263fa049b81c64793bac
Mayur Bhanushali <mayurb96@gmail.com>
2023-07-31 11:44:12 +0530

    Sending all files to github

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React,{ Fragment} from 'react';
+import classes from './Header.module.css';
+
+import mealsImg from '../../assests/meals.jpg'
+import HeaderCartButton from './HeaderCartButton';
+
+const Header=props=>{
+return(
+    <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton onClick={props.onShowCart}/>
+
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImg} alt='Food Table'></img>
+        </div>
+    </Fragment>
+);
+};
+
+export default Header;
